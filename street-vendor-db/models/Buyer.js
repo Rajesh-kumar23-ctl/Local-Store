8b5cf77b453818af9a58ffcafb4509eb200dcc9d
Opjena <ojena643@gmail.com>
2025-07-27 01:17:16 +0530

    k

--- street-vendor-db/models/Buyer.js
+++ street-vendor-db/models/Buyer.js
@@ -6,9 +6,9 @@ const buyerSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   gmail: { type: String },
   location: {
-    pincode: { type: String },
-    city: { type: String },
-    state: { type: String },
+    pincode: { type: String ,required: true},
+    city: { type: String ,required: true},
+    state: { type: String ,required: true},
   },
   cart: { type: Array, default: [] },
   automaticOrder: {
